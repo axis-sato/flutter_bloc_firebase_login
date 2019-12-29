bd114befef9db3ae3c6f2a55fb4a088e104ca84a
sato <sato_m@mediaxis.jp>
2019-12-29 21:31:12 +0900

    Bloc Delegate

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_firebase_login/authentication_bloc/authentication_bloc.dart';
 import 'package:flutter_bloc_firebase_login/authentication_bloc/authentication_event.dart';
 import 'package:flutter_bloc_firebase_login/authentication_bloc/authentication_state.dart';
+import 'package:flutter_bloc_firebase_login/simple_bloc_delegate.dart';
 import 'package:flutter_bloc_firebase_login/user_repository.dart';
+import 'package:bloc/bloc.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  BlocSupervisor.delegate = SimpleBlocDelegate();
   final userRepository = UserRepository();
   runApp(
     BlocProvider(
@@ -25,7 +26,7 @@ class App extends StatelessWidget {
   final UserRepository _userRepository;
 
   App({Key key, @required UserRepository userRepository})
-      : assert(_userRepository != null),
+      : assert(userRepository != null),
         _userRepository = userRepository,
         super(key: key);
 
